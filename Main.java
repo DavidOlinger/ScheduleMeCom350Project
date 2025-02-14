6e271822e0ee03cf3c26aa818f148048d65bcfaf
DavidOlinger <126510514+DavidOlinger@users.noreply.github.com>
2025-02-14 13:58:38 -0500

    Initial commit with Gradle setup

---
+++ Main.java
@@ -0,0 +1,15 @@
+package schedule.mvp.com350;
+
+public class Main {
+
+//public courseDatabase
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        //runs the thing
+    }
+
+}
